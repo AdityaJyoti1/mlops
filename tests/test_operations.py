95cf66717a7cf4019bcd263f123560b396044067
OMKAR <>
2025-04-23 23:47:00 +0530

    Unit Test-new

---
+++ tests/test_operations.py
@@ -0,0 +1,16 @@
+from src.maths_operations import add, subtract
+
+def test_add():
+    """Test the add function."""    
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(1.5, 2.5) == 4.0
+
+def test_subtract():
+    """Test the subtract function."""    
+    assert subtract(2, 1) == 1
+    assert subtract(1, 1) == 0
+    assert subtract(0, 0) == 0
+    assert subtract(2.5, 1.5) == 1.0
+
